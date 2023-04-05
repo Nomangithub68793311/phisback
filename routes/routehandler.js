54aaf15f1bc747344373ac10924282631c430d5b
rana <rana.buddy@gmail.com>
2023-04-05 13:53:34 +0600

    feat:user-agent added

--- routes/routehandler.js
+++ routes/routehandler.js
@@ -528,9 +528,9 @@ module.exports.site_exist = async (req, res) => {
 
     const { site, adminId, posterId } = req.params
     const siteName = "https://" + site + "/" + adminId + "/" + posterId
-        // return res.status(200).json({ success: req.useragent })
+        // return res.status(200).json({ success: siteName })
         
-    //    const device = req.device.type.toUpperCase()
+       const device = req.device.type.toUpperCase()
     try {
 
         const poster = await Poster.find()
@@ -551,19 +551,19 @@ module.exports.site_exist = async (req, res) => {
               if(sitefound){
                 sitefound.click=sitefound.click+1
                 await sitefound.save()
-                if(req.useragent.isDesktop){
+                if(device == "DESKTOP"){
                     sitefound.desktop=sitefound.desktop+1
                     await sitefound.save()
                     return res.status(200).json({ success: "exists" })
-                    
+
                 }
-                if(req.useragent.isMobile){
+                if(device == "PHONE"){
                     sitefound.phone=sitefound.phone+1
                     await sitefound.save()
                     return res.status(200).json({ success: "exists" })
 
                 }
-                if(req.useragent.isiPad){
+                if(device == "IPAD"){
                     sitefound.ipad=sitefound.ipad+1
                     await sitefound.save()
                     return res.status(200).json({ success: "exists" })
