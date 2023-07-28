a2df8f10424b16867c95fd6f317e6a49f9564662
rana <rana.buddy@gmail.com>
2023-07-28 12:00:05 +0600

    feat:github updated

--- server.js
+++ server.js
@@ -23,15 +23,7 @@ app.use(device.capture());
 app.use(useragent.express());
 const server=http.createServer(app)
 
-// const mongouri='mongodb+srv://userSimon:SimonntHJ3322@cluster0.ckww6.mongodb.net/userData?retryWrites=true&w=majority';
-// const mongouri = 'mongodb://ranaDatabase:ahmedimran96yoo@ac-nql5qlw-shard-00-00.xer9vl6.mongodb.net:27017,ac-nql5qlw-shard-00-01.xer9vl6.mongodb.net:27017,ac-nql5qlw-shard-00-02.xer9vl6.mongodb.net:27017/myfirstdatabase?ssl=true&replicaSet=atlas-o00q1f-shard-0&authSource=admin&retryWrites=true&w=majority'
-// const mongouri='mongodb://contact:My9J9xnpsYSRnH6@cluster0-shard-00-00.ncmj4.mongodb.net:27017,cluster0-shard-00-01.ncmj4.mongodb.net:27017,cluster0-shard-00-02.ncmj4.mongodb.net:27017/shannonDatabase?ssl=true&replicaSet=atlas-10vj3b-shard-0&authSource=admin&retryWrites=true&w=majority'
-// mongoose.connect(mongouri, {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// }).then((result) => {
-//     console.log('mongo connected');
-// })
-//     .catch((err) => { console.log(err) });
+
 connectDB()
 
 // const io = new Server(server, {
