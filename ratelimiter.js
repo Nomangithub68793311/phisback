8faea0fbc3e82b8521d73bb640f90098fe27dc47
rana <rana.buddy@gmail.com>
2023-11-30 15:24:06 +0600

    add

--- ratelimiter.js
+++ ratelimiter.js
@@ -1,7 +1,7 @@
 import setRateLimit from 'express-rate-limit';
 
 const rateLimitMiddleware = setRateLimit({
-    windowMs: 5 * 60 * 1000, 
+    windowMs: 1 * 60 * 1000, 
 	max: 2,
     message: "Hold on their, maybe get a life instead of spamming my api.",
 	standardHeaders: true,
